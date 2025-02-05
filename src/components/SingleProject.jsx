b2265e1beb6e0862d1a00a4e38fcf6a978838ec1
Ryan Bascos <rbascos@uci.edu>
2025-02-04 17:32:37 -0800

    Title Within Image of Project Page
    Added title inside of hero image at the bottom left. To do: make separate pages for the two projects, and fill each page with a respective project

--- src/components/SingleProject.jsx
+++ src/components/SingleProject.jsx
@@ -3,15 +3,23 @@ import { Navbar } from '../components'
 
 const SingleProject = () => {
   return (
-    <div className='flex-column vw-100 d-flex justify-content-center border border-primary'>
+    <div>
       <Navbar/>
-      <img src={landscape_pic} className=''/>
+      <div>
+        <img 
+          src={landscape_pic} 
+          className='vw-100 vh-100 object-fit-cover'
+        />
+        <h1 className="position-absolute bottom-0 text-white p-4 bg-dark bg-opacity-50">
+          NART: The Petfinder Drone
+        </h1>
+      </div>
       <div className='m-5 px-lg-5'>
         <h1 className='mb-3'>
-          NART: The Petfinder Drone and Web App
+          Summary
         </h1>
         <p>
-          Hi! This right here is my project. I'm describing it now!
+          The user needs to upload their pics to the app. The app sends the pics to the drone's onboard object detection model. The model will send a notification back to the web app when a potential match for the pet is found.
         </p>
       </div>
     </div>
